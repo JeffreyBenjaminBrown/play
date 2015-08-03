4935ccc8a27ca13c09dbaaed5ed6ac2eee3f108a
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2015-08-02 17:37:59 -0700

    add readme

---
+++ tictactoe/README.md
@@ -0,0 +1,5 @@
+Jake Brownson wrote this and posted it here:
+  https://gist.github.com/jbrownson/712ddafd1cb3388cd827
+I mostly just added comments.
+
+
--- tictactoe/it.hs
+++ tictactoe/it.hs
@@ -1,7 +1,3 @@
--- Jake Brownson wrote this and posted it here:
-  -- https://gist.github.com/jbrownson/712ddafd1cb3388cd827
--- I mostly just added comments.
-
 import Data.List
 import Data.Maybe
 import Control.Monad.State
