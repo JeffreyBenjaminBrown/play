1a11b55b62f376a9497ec2ef0ae15ff6be957060
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2015-12-18 02:05:53 -0800

    emulating Jim Zamerski, scored the first 90 digits of sqrt 2

---
+++ digits/root_2.hs
@@ -0,0 +1,23 @@
+copied from Wolfram Alpha and once verified:
+  ask for "root 2 in base 10"
+  verified 6 digits at a time
+1.4b79170a07b85737704b085486853504563650b559b8b79a401387b342380a998a173
+a951303434821b55419a068816958b64282342a358a8947369b97237b9b
+04b656a0723349328a219013a8b21ab42844a5758ba27b3a14317b17b28
+
+
+the first 90 digits, set to chords!
+F = french chord = 0 4 6 10
+d = dim, hd = half dim, pd = phr dom = 0 1 4 5 7 8 10
+1.4b 791 70a07 b85 737  7 (coda)
+9M   4m  0m*2  5hd 7M7  3M
+
+04b 085 48  68  535 045 636 50   b5  59 b8b 
+0M  5m7 1m7 6M7 5m  5M  0hd 5lyd 5wh aM bd
+
+79a  4    01 387 b34  238 0a9 98a17 
+0m7  0M7  5M 8M  8mM7 8M      7d
+
+3a9 513 03 43 48 21 b554 19 (da coda)
+3m  5pd 5m 3d 4M 2M bhd  9M
+
