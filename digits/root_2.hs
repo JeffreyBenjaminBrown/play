aa495b4dbb1d2c4e21be942791abdf00881bd655
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2015-12-18 15:51:25 -0800

    root 2: score the first 188 digits!

--- digits/root_2.hs
+++ digits/root_2.hs
@@ -1,23 +1,33 @@
-copied from Wolfram Alpha and once verified:
-  ask for "root 2 in base 10"
+the digits, copied from Wolfram Alpha and once verified:
+  asked for "root 2 in base 10"
   verified 6 digits at a time
+  these are the first four lines
+    as appear in the browser when line length is at max
 1.4b79170a07b85737704b085486853504563650b559b8b79a401387b342380a998a173
 a951303434821b55419a068816958b64282342a358a8947369b97237b9b
 04b656a0723349328a219013a8b21ab42844a5758ba27b3a14317b17b28
+a4354b796260136269a55a79598a4619ba2352a310a3373251b059876b
 
-
-the first 90 digits, set to chords!
+the first 188 digits, set to chords!
 F = french chord = 0 4 6 10
 d = dim, hd = half dim, pd = phr dom = 0 1 4 5 7 8 10
-1.4b 791 70a07 b85 737  7 (coda)
-9M   4m  0m*2  5hd 7M7  3M
+mx = mixolydian, _ = postfix drop octave
+dr = dorian
+1.4b 791 7  0a07 b85 737  7   04b 085 4*8 68
+9M   4m  0m 0M   5hd 3M   7mx 0M  5m7 1m7,67
+  *coda. end on 0M, not 1m7.
+535 045 636 50b 559 b8b
+5m  5M  0hd 5ly 5wh bd
+79a  401 387 b34  238 0a9 98a_1
+07   5M  8M  6m7  8M      7d
+73a9 513 03,43 48,21 b554 19 (da coda?)
+37   5pd 5m,3d 4M,4m bhd  9M
 
-04b 085 48  68  535 045 636 50   b5  59 b8b 
-0M  5m7 1m7 6M7 5m  5M  0hd 5lyd 5wh aM bd
-
-79a  4    01 387 b34  238 0a9 98a17 
-0m7  0M7  5M 8M  8mM7 8M      7d
-
-3a9 513 03 43 48 21 b554 19 (da coda)
-3m  5pd 5m 3d 4M 2M bhd  9M
-
+a068 81 695 8b6 428,234 2a3 58a 894 736 9b97 237 
+87   1M 6d  5d  4wh     3m  5hd 6hd 9hd 7M   0m
+b9b 04b 656a 072 33 493 28a 219 
+7M  0M  6M   0M  0m 07  2hd 4dr (dc?)
+013a 8b21ab 42844a 5758ba2
+am   8m     4w 2w  37
+7  b3a 1431 7b1 7b2 8  (dc)
+7m 8m  1m   1hd 77  47
