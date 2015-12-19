4dc92b119a67f8dd539be86bdff6c3e446bec5aa
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2015-12-18 19:01:26 -0800

    clarify lang

--- digits/root_2.hs
+++ digits/root_2.hs
@@ -1,6 +1,6 @@
-the digits, copied from Wolfram Alpha and once verified:
+the digits, copied from Wolfram Alpha
   asked for "root 2 in base 10"
-  verified 6 digits at a time
+  after copying, verified 6 digits at a time
   these are the first four lines
     as appear in the browser when line length is at max
 1.4b79170a07b85737704b085486853504563650b559b8b79a401387b342380a998a173
@@ -8,16 +8,30 @@ a951303434821b55419a068816958b64282342a358a8947369b97237b9b
 04b656a0723349328a219013a8b21ab42844a5758ba27b3a14317b17b28
 a4354b796260136269a55a79598a4619ba2352a310a3373251b059876b
 
-the first 188 digits, set to chords!
-F = french chord = 0 4 6 10
-d = dim, hd = half dim, pd = phr dom = 0 1 4 5 7 8 10
-mx = mixolydian, _ = postfix drop octave
+set to chords!
+!the first 188 digits of root 2 base 12
+=====================================================
+The order on the lines
+  The first in each pair is the melody.
+  Under each melody line is chords.
+
+M=major, m=minor, d = dim, hd = half dim
+pd = phrygian dominant = 0 1 4 5 7 8 10
+_ = postfix drop octave (operates on single melody notes)
+7 = mixolydian. (Sorry; this notation predates me.)
 dr = dorian
-1.4b 791 7  0a07 b85 737  7   04b 085 4*8 68
-9M   4m  0m 0M   5hd 3M   7mx 0M  5m7 1m7,67
-  *coda. end on 0M, not 1m7.
+ly = lydian
+(,) = these are to be played (faster) as one
+dc = da coda
+  coda = the end after beginning a second time
+  da = to? "da coda" is italian, I think, for 
+    "go through the first part again"
+
+1.4b 791 7  0a07 b85 737  7  04b 085 4*8 68
+9M   4m  0m 0M   5hd 3M   77 0M  5m7 1m7*,67
+  *coda: end on 0M, not 1m7
 535 045 636 50b 559 b8b
-5m  5M  0hd 5ly 5wh bd
+5m  5M  0hd 5ly 5w  bd
 79a  401 387 b34  238 0a9 98a_1
 07   5M  8M  6m7  8M      7d
 73a9 513 03,43 48,21 b554 19 (da coda?)
