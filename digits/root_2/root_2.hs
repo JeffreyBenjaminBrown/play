c990df132cc8423ab063eb779703aa295888d6fc
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2015-12-19 13:42:37 -0800

    divide, more. some lost data (simplified chords).

---
+++ digits/root_2/root_2.hs
@@ -0,0 +1,57 @@
+1.4b 791 7  0a07 b85 737  7  04b 085 4*8 68
+9M   4m  0m 0M   5hd 3M   7x 0M  5m7 1m7*,6x
+  *coda: end on 0M, not 1m7
+
+535 045 636 50b 5'5'9 b8'b
+5m  5M  0hd 5ly 5w    bd
+
+79a_ 401 387 b34  238 0a9 98a_1
+0x   5M  8M  6m7  8M      7d
+
+73a 95 13   03,43  48,21  b554 19 (da coda?)
+5x   ~ a(9) 5m,3d 2ly,4dr bhd  9M
+
+a0 68 81 695 8* (b642 8234) 2a3 58a 894 736 9b97 237 
+8x    1M 6d     (4sus 4x  )  3  aM   4   b   7M   0
+  *isolated because can belong either side
+    because each should feel unit  
+
+b9b0 4b 656a..   072    3  3*    
+7M   0M 6(s:1 6) 5(09)  a8 3(a7)
+  *or delete this chord, subsume note into next phrase
+
+RESUME HERE
+
+49 328a 219..
+07 2hd  4dr (dc?)
+
+013a 8b21ab 42844a 5758ba2
+am   8m     4w 2w  37
+
+7  b3a 1431 7b1 7b2 8  (dc)
+7m 8m  1m   1hd 77  47
+
+a43 54b 7962 6013 
+
+6269 a55a 7959 8a4619ba
+
+2352a 310a337325
+
+1b05 987,6b
+5    ?   b
+
+4537  68  1.a1 91   a69 015 60
+3x?0m 8x  1M   5au ~    6ly 0fr
+
+b1336 295 3a_3   b_37
+bM    2m  3(3 9) b(3 9)
+
+054 25  159369305 
+0   b05 
+
+141 04 dc
+4x  4w
+
+256565
+
+
