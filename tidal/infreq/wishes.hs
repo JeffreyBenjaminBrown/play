c5fb80ef1dbaefa8fbcd7e5b846e8608cf373701
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2016-05-02 19:48:24 -0700

    These were copied already to a subdirectory.

--- tidal/infreq/wishes.hs
+++
@@ -1,8 +0,0 @@
-data Cycle { duration :: Fractional a => a
-           , events :: [ ? OscPattern ]
-             -- sound :: Pattern String -> OscPattern
-           }
-
--- define and use like a sample name in a pattern something that means to put multiple samples through the same instructions
-sound $ "bd bass hit sn" where hit = "[bd, bass, sn]" 
-
