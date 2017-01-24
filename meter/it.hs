23ad443f23bb86aff1efd0db8dbdda27b495fd5b
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2017-01-24 00:12:26 -0800

    A Meter inputs a list of numbers, interprets them like [first quarternote, third sixteenth note], and outputs a time.

---
+++ meter/it.hs
@@ -0,0 +1,23 @@
+import Data.Ratio
+
+type Dur = Rational -- duration
+type When = Rational -- the time something happens
+type Count = Rational -- how many durations
+
+type Meter = [Count] -> When
+  -- TODO: inverseMeter :: Meter -> When -> [Count]
+    -- done the obvious way, this might not terminate, if the When is from a different Meter
+
+straight :: Count -> Meter
+straight divisor = sum . snd 
+  $ foldl (\(unitLength, lengths) nUnits
+            -> ( unitLength / divisor
+               , unitLength * nUnits : lengths))
+      (1 / divisor,[])
+  -- examples
+    -- straight 4 [0] = 0
+    -- straight 4 [1] = 1 % 4
+    -- straight 4 [0,1] = 1 % 16
+    -- straight 4 [1,1] = 5 % 16
+    -- straight 3 [0,0,1] = 1 % 27
+    -- etc.
