4f67fd8dccccda1a99116a46f6f2a0b99a0ee268
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2017-07-02 15:21:06 -0700

    fix a sign error, refactor

--- _not_music/jbrownson_tictactoe/simpler.hs
+++ _not_music/jbrownson_tictactoe/simpler.hs
@@ -1,23 +1,26 @@
 import Data.Maybe
-import Control.Lens
+import Control.Lens (ix, (.~), (&))
 
 data XO = X | O deriving (Show, Eq)
 type Cell = Maybe XO
 type Coord = Int
 type Board = [Cell]
 
-score :: XO -> Board -> Int
-score x b | won x b = 1
-          | full b = 0
-          | otherwise = negate
-                        $ minimum
-                        $ map (score $ notXO x)
-                        $ tryEverything (notXO x) b
+score :: XO -> Board -> Int  -- the value to xo of moving the board to state b
+score xo b | won xo b = 1
+           | full b = 0
+           | otherwise = negate
+                         $ maximum
+                         $ map (score opponent)
+                         $ tryEverything opponent b
+  where opponent = notXO xo
+
+tweakList :: Int -> a -> [a] -> [a] -- change one element
+tweakList position newMember original =
+  original & ix position .~ newMember -- Lens magic, not critical
 
 tryEverything :: XO -> Board -> [Board]
-tryEverything x b = map f $ openSpots b
-  where f :: Int -> Board
-        f spot = b & ix spot .~ (Just x)
+tryEverything xo b = map (\spot -> tweakList spot (Just xo) b) $ openSpots b
 
 openSpots :: [Maybe a] -> [Int]
 openSpots list = map fst $ filter (isNothing . snd) $ zip [0..] list
@@ -26,17 +29,17 @@ full :: Board -> Bool
 full = and . map (not . isNothing) 
 
 won :: XO -> Board -> Bool
-won x b = hasTwoInARow $ map f b
+won xo b = hasTwoInARow $ map f b
   where f Nothing = Nothing
-        f (Just y) | y == x = Just y
+        f (Just y) | y == xo = Just y
                    | otherwise = Nothing
+
+hasTwoInARow :: [Maybe a] -> Bool
+hasTwoInARow list = or $ map pred listWithLag
+  where listWithLag = zip list (Nothing : list)
+        pred (Just _, Just _) = True
+        pred _ = False
 
 notXO :: XO -> XO
 notXO X = O
 notXO O = X
-
-hasTwoInARow :: [Maybe a] -> Bool
-hasTwoInARow list = or $ map pred x
-  where x = zip list (Nothing : list)
-        pred (Just _, Just _) = True
-        pred _ = False
