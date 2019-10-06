0ecc7e18386d1477080c93601f1a657dc2aa0a10
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-10-06 17:28:07 -0500

    simplify, thanks to NixOS

--- dot-emacs.el
+++ dot-emacs.el
@@ -1,29 +1,30 @@
-; jbb-custom macros
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
 
+; jbb-custom macros
 ;;  How to store macros here:
 ;;    use F3, then F4 to record the macro
 ;;    M-x name-last-kbd-macro
 ;;    M-x insert-kbd-macro
 ;;    paste that code below
 ;;    maybe also give it a name (per examples below)
+
 (defun shorten-other-window ()
   "Expand current window to use a bit more than half of the other window's lines."
   (interactive)
-  (enlarge-window (floor (* (window-height (next-window)) 0.55))))
+  (enlarge-window (floor (* (window-height (next-window)) 0.45))))
 
-;; add-to-list 'auto-mode-alist '("\\.curry\\'" . haskell-mode))
+(add-to-list 'auto-mode-alist '("\\.nix\\'" . nix-mode))
 
-;; (load-file "~/.emacs.d/elisp/coconut-mode.el")
-
-;; (global-auto-revert-mode t) ;; reload files when changed
+(global-auto-revert-mode t) ;; reload files when changed
 
 ;; no tabs
 (setq-default indent-tabs-mode nil)
 (setq tab-width 2)
 (setq python-indent 2)
-
-;; use Prolog not Perl in .pl files
-(add-to-list 'auto-mode-alist '("\\.\\(pl\\|pro\\|lgt\\)" . prolog-mode))
 
 ;; jbb macros
 (global-set-key (kbd "C-c a") 'append-to-file)
@@ -33,7 +34,7 @@
 ;; makes debugging easier. josh suggests
   (add-hook 'after-init-hook '(lambda () (setq debug-on-error t)))
 
-;; kill-region should have no effect if the region is not activeo
+;; kill-region should have no effect if the region is not active
   (defvar mark-even-if-inactive nil)
 
 ;;;; Semantic Synchrony
@@ -48,36 +49,20 @@
 ;;    (normal-top-level-add-subdirs-to-load-path))
 ;;  (require 'smsn-mode)
 
-;; haskell
-  ;; there's also haskell-emacs, installed via `M-x package-list-packages`
-  ;; installing the haskell-mode package from within emacs
-    ;; required me to then add this
-  (add-to-list 'load-path "~/.emacs.d/elpa/haskell-mode-16.1")
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("8b82aa6c511e289ed6868ae9c082b04844968be6ce2eef62c3c899b2b1038eb4" default))
- '(haskell-process-path-stack "/home/jeff/code/Tidal")
- '(haskell-process-type 'stack-ghci)
+   ;; Was added automatically the first time I edited the manoj-dark theme.
+   ;; Prevents Emacs from asking whether it's safe.
+   (quote
+    ("cf7ed2618df675fdd07e64d5c84b32031ec97a8f84bfd7cc997938ad8fa0799f" default)))
  '(package-archives
-   '(("gnu" . "http://elpa.gnu.org/packages/")
-     ("melpa-stable" . "http://stable.melpa.org/packages/")))
- '(package-selected-packages
-   '(groovy-mode intero discover-my-major discover yafolding find-file-in-repository rainbow-delimiters hide-lines idris-mode ac-haskell-process haskell-emacs-base ess haskell-emacs haskell-mode))
- '(send-mail-function 'mailclient-send-it))
-    ;; per advice: https://mail.google.com/mail/u/0/#inbox/15fc2b4e97f194d2
-  (require 'haskell-mode)
-  (require 'package)  ;; (following https://github.com/haskell/haskell-mode readme)
-
-;; tidal
-  (add-to-list 'load-path "~/code/Tidal")
-  (add-to-list 'load-path "~/code/git_play/tidal/emacs")
-  (require 'tidal)
-   ;; doesn't seem to help
+   (quote
+    (("gnu" . "http://elpa.gnu.org/packages/")
+     ("melpa-stable" . "http://stable.melpa.org/packages/")))))
 
 ;; custom, jbb
   (show-paren-mode 1)
@@ -134,16 +119,16 @@
 (put 'upcase-region 'disabled nil)
 (put 'scroll-left 'disabled nil)
 
-(add-to-list 'load-path "/usr/local/mercury-14.01.1/lib/mercury/elisp")
-(autoload 'mdb "gud" "Invoke the Mercury debugger" t)
+;;(add-to-list 'load-path "/usr/local/mercury-14.01.1/lib/mercury/elisp")
+;;(autoload 'mdb "gud" "Invoke the Mercury debugger" t)
 
 ;; Intero, the Haskell IDE: http://chrisdone.github.io/intero/
 ;; (add-hook 'haskell-mode-hook 'intero-mode)
 
 ;; elpy, the emacs python environment
-(setq elpy-rpc-python-command "python3")
-(elpy-enable)
+;;(setq elpy-rpc-python-command "python3")
+;;(elpy-enable)
 
 ;; exec-path-from-shell, which package makes Emacs's PATH match shell's
-(when (memq window-system '(mac ns x))
-  (exec-path-from-shell-initialize))
+;;(when (memq window-system '(mac ns x))
+;;  (exec-path-from-shell-initialize))
