500ef4a1ef8eb4336ac1e3ba695f96a59a002524
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2021-07-21 17:10:38 -0500

    delete an unused function

--- dot-emacs.el
+++ dot-emacs.el
@@ -272,33 +272,6 @@ PITFALL: If there are no leaves, the regex search will fail, and an error messag
         regexp-history)
   (call-interactively 'org-roam-insert))
 
-(defun org-roam-create-note-from-headline ()
-  ;; by user 'telotortium' at the org-roam discourse:
-  ;; https://org-roam.discourse.group/t/creating-an-org-roam-note-from-an-existing-headline/978
-  "Create an Org-roam note from the current headline and jump to it.
-
-  Normally, insert the headline’s title using the ’#title:’
-  file-level property
-  and delete the Org-mode headline. However, if the current headline has a
-  Org-mode properties drawer already, keep the headline and don’t insert
-  ‘#+title:'. Org-roam can extract the title from both kinds of notes,
-  but using
-  ‘#+title:’ is a bit cleaner for a short note, which Org-roam encourages."
-  (interactive)
-  (let ((title (nth 4 (org-heading-components)))
-        (has-properties (org-get-property-block)))
-    (org-cut-subtree)
-    (org-roam-find-file title nil nil 'no-confirm)
-    (org-paste-subtree)
-    (unless has-properties
-      (kill-line)
-      (while (outline-next-heading)
-        (org-promote)))
-    (goto-char (point-min))
-    (when has-properties
-      (kill-line)
-      (kill-line))))
-
 ;; Doesn't work but would be cool.
 (defun bms/org-roam-rg-search ()
   ;; https://org-roam.discourse.group/t/using-consult-ripgrep-with-org-roam-for-searching-notes/1226
