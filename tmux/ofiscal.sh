b64f32412456f5e5260ddc29b7c4668a2a02e07e
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-02-06 10:54:53 -0500

    tmux for ofiscal:pensions

--- tmux/ofiscal.sh
+++ tmux/ofiscal.sh
@@ -12,6 +12,26 @@ tmux -L of send-keys -t web:0 \
 
 
 ###
+### Setup pensions
+###
+
+docker start pension
+
+tmux -L of send-keys -t pensions:0     \
+  "docker exec -it pension bash" Enter \
+  "cd mnt/"                      Enter
+
+tmux -L of send-keys -t pensions:1     \
+  "docker exec -it pension bash" Enter \
+  "cd mnt/"                      Enter \
+  "ipython3"                     Enter
+
+tmux -L of send-keys -t pensions:2 \
+     "ssh-agent bash" Enter        \
+     "ssh-add"        Enter
+
+
+###
 ### Setup tax.co.web
 ###
 
